b9ecc0afb72f072c64d213e9ef984c2c7c9a95a8
Mickaël Schoentgen <contact@tiger-222.fr>
2025-12-07 20:23:47 +0100

    feat: more Zip test files

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,9 +3,12 @@
 echo " ZIP"
 for file in files/zip/*.test; do
     echo -n " - ${file} ... "
-    ./zipcracker-ng -f "${file}" -w list/10k-most-common.txt >/dev/null && echo -n "\033[0;32mOK\033[0m " || echo -n "\033[0;31mERROR\033[0m "
-    ./zipcracker-ng -f "${file}" -w list/10k-most-common-crlf.txt >/dev/null && echo "\033[0;32mOK\033[0m" || echo "\033[0;31mERROR\033[0m"
+    ./zipcracker-ng -f "${file}" -w list/10k-most-common.txt >/dev/null 2>&1 && echo -n "\033[0;32mOK\033[0m " || echo -n "\033[0;31mERROR\033[0m "
+    ./zipcracker-ng -f "${file}" -w list/10k-most-common-crlf.txt >/dev/null 2>&1 && echo "\033[0;32mOK\033[0m" || echo "\033[0;31mERROR\033[0m"
 done
+
+echo -n " - files/zip/protected.zip ... "
+echo "sample-files" | ./zipcracker-ng -f "files/zip/protected.zip" >/dev/null && echo "\033[0;32mOK\033[0m" || echo "\033[0;31mERROR\033[0m"
 
 echo
 echo "CPT"
